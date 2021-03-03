6b2285a396a26fd96c444f3d5a527ee0444f2971
daisuke saito <dsaito85a@gmail.com>
2021-03-03 14:17:13 +0900

    エンティティーマネージャーの使用

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,18 @@ createConnection({
     ],
     synchronize: true,
     logging: false
-}).then(connection => {
-    // here you can start to work with your entities
+}).then( async connection => {
+
+    let photo = new Photo();
+    photo.name = "Me and Bears";
+    photo.description = "I am near polar bears";
+    photo.filename = "photo-with-bears.jpg";
+    photo.views = 1;
+    photo.isPublished = true;
+
+    await connection.manager.save(photo);
+    console.log("Photo has been saved. Photo id is", photo.id);
+
+    let savedPhotos = await connection.manager.find(Photo);
+    console.log("All photos from the db: ", savedPhotos);
 }).catch(error => console.log(error));
